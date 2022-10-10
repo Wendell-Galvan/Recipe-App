f4a629346fcacc96c048b2501aac5b7b5a0f3f0d
Wendell Galvan <you@example.com>
2022-10-10 14:08:31 -0700

    fixed function arguments

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ let mealURL = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 axios.get(mealURL)
   .then((response) => {
-      console.log(response.data.meals[0]);
+      console.log(response.data);
       displayMeal(response.data);
      })
 
-  function displayMeal(data){
+
+
+function displayMeal(data){
      const meal = data.meals[0];
      const mealDiv = document.getElementById('displayMeal');
+     const ingredients = document.createElement("ul");
 
      //show meal name 
      const mealName = meal.strMeal;
@@ -16,17 +19,29 @@ axios.get(mealURL)
      heading.innerHTML = mealName;
      mealDiv.appendChild(heading);
 
-     //display meal image 
+     displayImage(meal, mealDiv);
+     displayIngred(meal, mealDiv, ingredients);
+     getIngred(meal, mealDiv, ingredients);
+     mealInstruct(meal, mealDiv);
+     youTubeBtn(meal, mealDiv);
+}
+
+//display meal image 
+function displayImage(meal, mealDiv){
      const mealImg = document.createElement("img");
      mealImg.src = meal.strMealThumb;
      mealDiv.appendChild(mealImg);
      document.body.style.backgroundImage = meal.strMealThumb;
+}
 
-     //show ingredients on UI 
-     const ingredients = document.createElement("ul");
+//show ingredients on UI 
+function displayIngred(meal, mealDiv, ingredients){
+     // const ingredients = document.createElement("ul");
      mealDiv.appendChild(ingredients);
+}
 
-     //Ingredient list from API 
+//Ingredient list from API
+function getIngred(meal, mealDiv, ingredients){ 
      const getIngredients = Object.keys(meal)
           .filter(function (ingredient){
                return ingredient.indexOf("strIngredient") == 0;
@@ -44,15 +59,20 @@ axios.get(mealURL)
               listItem.innerHTML = value;
               ingredients.appendChild(listItem);
             } 
+}
 
-     //Instruction on how to make meal
+
+     
+//Instruction on how to make meal
+function mealInstruct(meal, mealDiv){
      const mealInstructions = meal.strInstructions;
      const paragraph = document.createElement("p");
      paragraph.innerHTML = mealInstructions;
      mealDiv.appendChild(paragraph);
-
+}
 
      //Link to Youtube
+function youTubeBtn(meal, mealDiv){
      const vidBtn = document.createElement("button");
      vidBtn.setAttribute('id', 'button');
      vidBtn.textContent = "Youtube";
@@ -65,8 +85,8 @@ axios.get(mealURL)
           // const mealVid = meal.strYoutube;
           location.href = mealVid;
      }
+}
 
-  }
 
   //New recipe button
 
